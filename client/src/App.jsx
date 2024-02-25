2427a4a622cb620b745218408d9212cb7de9f727
leansttar <105887401+leannsttar@users.noreply.github.com>
2024-02-24 22:45:46 -0600

    Creación y organización del proyecto

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-red-300'>The app</h1>
+    </>
+  )
+}
+
+export default App
