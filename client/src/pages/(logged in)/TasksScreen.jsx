14716cccb81092168c9a79154a11fcbbd0209aa8
Rodri Pineda <105886719+Rodrogas666@users.noreply.github.com>
2024-04-28 16:37:25 -0600

    Update TasksScreen.jsx

--- client/src/pages/(logged in)/TasksScreen.jsx
+++ client/src/pages/(logged in)/TasksScreen.jsx
@@ -5,12 +5,60 @@ import { CiCalendarDate } from "react-icons/ci";
 
 export const TasksScreen = () => {
   return (
-    <div className="w-full min-h-full max-h-full overflow-y-scroll bg-[#f7f7f7] flex flex-col">
-      <p>Tasks</p>
+    <div className="w-full min-h-full overflow-y-scroll bg-[#f7f7f7] grid grid-cols-1 md:grid-cols-3 font-Poppins">
+      <div className="bg-[#FFFFFF] shadow-lg m-8 p-4 max-h-[55vh]">
+        <div className="flex justify-between items-center mb-6">
+          <p className="text-xl font-bold">Por hacer</p>
+          <div className="flex gap-2">
+            <CiCirclePlus size={24} />
+            <HiDotsHorizontal size={24} />
+          </div>
+        </div>
+        <div className="flex flex-row gap-2 mb-4">
+          <p className="bg-[#EBFAE2] text-[#4F9C20] rounded p-1 font-semibold">
+            Feedback
+          </p>
+          <p className="bg-[#FDF2F2] text-[#EC5962] rounded p-1 font-semibold">
+            Bug
+          </p>
+        </div>
+        <div className="mb-4">
+          <p className="font-bold text-lg">Improve cards readability</p>
+        </div>
+        <div className="mb-4 flex justify-center">
+          <img
+            src="../../../public/test-task.png"
+            className="w-full max-w-xs md:max-w-full h-auto"
+            alt=""
+          />
+        </div>
+        <div className="mb-4 flex justify-between">
+          <div className="flex items-center">
+            <CiCalendarDate size={24} />
+            <p className="ml-2">Date 12/12/12</p>
+          </div>
+          <p className="text-right font-medium">Team C</p>
+        </div>
+        <div className="flex items-center">
+          <img
+            src="../../../public/test-boy.png"
+            className="w-8 h-8 mr-2"
+            alt=""
+          />
+          <div className="flex items-center">
+            <LiaCommentSolid size={24} />
+            <p className="ml-1">
+              <span>12 </span>Comentarios
+            </p>
+          </div>
+        </div>
+        <div
+          className="w-full overflow-auto"
+          style={{ height: "calc(100% - 10rem)" }}
+        >
+          <p>Tasks</p>
+        </div>
+      </div>
     </div>
-  </div>
-  
-
-    
   );
 };
