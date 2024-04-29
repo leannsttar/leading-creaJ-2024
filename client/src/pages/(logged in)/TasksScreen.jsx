037c252c77dc03216f29e5518a09c81f8ee4f7b8
Rodri Pineda <105886719+Rodrogas666@users.noreply.github.com>
2024-04-28 20:12:38 -0600

    Test de reorganización

--- client/src/pages/(logged in)/TasksScreen.jsx
+++ client/src/pages/(logged in)/TasksScreen.jsx
@@ -1,17 +1,7 @@
-import TaskCard from "./TaskCard";
+import { Outlet } from "react-router-dom";
 
 export const TasksScreen = () => {
   return (
-    <div className="w-full min-h-full overflow-y-scroll bg-[#f7f7f7] grid grid-cols-1 md:grid-cols-3 font-Poppins">
-      <TaskCard
-        title="Por hacer"
-        subtitle="Improve cards readability"
-        imageSrc="../../../public/test-task.png"
-        date="Date 12/12/12"
-        team="Team C"
-        comments={12}
-      />
-      {/* Para las otras col aquí irán */}
-    </div>
+    <Outlet/>
   );
 };
--- client/src/pages/(logged in)/tasksTabs/TasksBoardTab.jsx
+++ client/src/pages/(logged in)/tasksTabs/TasksBoardTab.jsx
@@ -1,56 +1,22 @@
+import TaskCard from "../TaskCard";
+import React from "react";
+
 export const TasksBoardTab = () => {
-    return (
-      <div className="w-full overflow-auto pt-[6rem] md:pt-[3rem] lg:pt-0 pb-[2rem] "
+  return (
+    <div
+      className="w-full overflow-auto pt-[6rem] md:pt-[3rem] lg:pt-0 pb-[2rem] "
       style={{ height: "calc(100% - 10rem)" }}>
-        <p>Board tab</p>
-        <p>Board tab</p>
-        <p>Board tab</p>
-        <p>Board tab</p>
-        <p>Board tab</p>
-        <p>Board tab</p>
-        <p>Board tab</p>
-        <p>Board tab</p>
-        <p>Board tab</p>
-        <p>Board tab</p>
-        <p>Board tab</p>
-        <p>Board tab</p>
-        <p>Board tab</p>
-        <p>Board tab</p>
-        <p>Board tab</p>
-        <p>Board tab</p>
-        <p>Board tab</p>
-        <p>Board tab</p>
-        <p>Board tab</p>
-        <p>Board tab</p>
-        <p>Board tab</p>
-        <p>Board tab</p>
-        <p>Board tab</p>
-        <p>Board tab</p>
-        <p>Board tab</p>
-        <p>Board tab</p>
-        <p>Board tab</p>
-        <p>Board tab</p>
-        <p>Board tab</p>
-        <p>Board tab</p>
-        <p>Board tab</p>
-        <p>Board tab</p>
-        <p>Board tab</p>
-        <p>Board tab</p>
-        <p>Board tab</p>
-        <p>Board tab</p>
-        <p>Board tab</p>
-        <p>Board tab</p>
-        <p>Board tab</p>
-        <p>Board tab</p>
-        <p>Board tab</p>
-        <p>Board tab</p>
-        <p>Board tab</p>
-        <p>Board tab</p>
-        <p>Board tab</p>
-        <p>Board tab</p>
-        <p>Board tab</p>
-        <p>Board tab</p>
+      <div className="w-full min-h-full overflow-y-scroll bg-[#f7f7f7] grid grid-cols-1 md:grid-cols-3 font-Poppins">
+        <TaskCard
+          title="Por hacer"
+          subtitle="Improve cards readability"
+          imageSrc="../../../public/test-task.png"
+          date="Date 12/12/12"
+          team="Team C"
+          comments={12}
+        />
+        {/* Para las otras col aquí irán */}
       </div>
-    );
-  };
-  
+    </div>
+  );
+};
