27c5fefebc2f56b616c0b69a40624d6964bdb31c
Rodri Pineda <105886719+Rodrogas666@users.noreply.github.com>
2024-04-28 21:01:32 -0600

    Prueba de calendario
    aún no me convence del todo, tendría que ver más a detalle sino, busco otro

--- client/src/pages/(logged in)/tasksTabs/TasksCalendarTab.jsx
+++ client/src/pages/(logged in)/tasksTabs/TasksCalendarTab.jsx
@@ -1,8 +1,20 @@
+import React, { useState } from "react";
+import { Calendar } from "@/components/ui/calendar";
+
 export const TasksCalendarTab = () => {
-    return (
-      <div className="w-full overflow-auto pt-[6rem] md:pt-[3rem] lg:pt-0 pb-[4rem] "
-      style={{ height: "calc(100% - 10rem)" }}>
-        <p>Calendar tab</p>
-      </div>
-    );
-  };
+  const [date, setDate] = useState(new Date());
+
+  return (
+    <div
+      className="w-full overflow-auto pt-[6rem] md:pt-[3rem] lg:pt-0 pb-[4rem] "
+      style={{ height: "calc(100% - 10rem)" }}
+    >
+      <Calendar
+        mode="single"
+        selected={date}
+        onSelect={setDate}
+        className="rounded-md border"
+      />
+    </div>
+  );
+};
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: ["class"],
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     screens: {
       'sm': '320px',
       'md': '768px',
@@ -23,7 +31,22 @@ export default {
     daisyui: {
       themes: ["light", "dark", "cupcake"],
     },
+    keyframes: {
+      "accordion-down": {
+        from: { height: "0" },
+        to: { height: "var(--radix-accordion-content-height)" },
+      },
+      "accordion-up": {
+        from: { height: "var(--radix-accordion-content-height)" },
+        to: { height: "0" },
+      },
+    },
+    animation: {
+      "accordion-down": "accordion-down 0.2s ease-out",
+      "accordion-up": "accordion-up 0.2s ease-out",
+    },
   },
-  plugins: [require("daisyui")],
+  plugins: [require("daisyui"), require("tailwindcss-animate")],
+  
 }
 
